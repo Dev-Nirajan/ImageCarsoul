5d72a47e2670acbeb63978330116e7311404bc0d
J-Neer <jnirajan050@gmail.com>
2025-12-23 20:34:16 +0545

    Make Better

--- src/NavButton.jsx
+++ src/NavButton.jsx
@@ -2,7 +2,7 @@ const NavButton = (props) => {
   const { icon, onClick, show } = props;
 
   return !show ? null : (
-    <button className=" flex justify-center bg-purple-800 rounded-lg w-14 m-2" onClick={onClick}>
+    <button className=" flex justify-center bg-purple-800 rounded-lg w-14 m-1" onClick={onClick}>
       <i
         className={`fa-solid fa-circle-chevron-${icon} text-purple-500 text-3xl py-4`}
       ></i>
